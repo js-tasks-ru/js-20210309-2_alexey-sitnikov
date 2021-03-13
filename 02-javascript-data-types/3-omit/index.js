4361a571c765cf5d7dca987b3a1024f7c9b83c3d
Sitnikov Alexey <a.sitnikov@bi.zone>
2021-03-13 22:01:44 +0300

    add solutions for 2-pick and 3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const entries = Object.entries(obj);
 
+  const pickedEntries = entries.filter(([key, _]) => !fields.includes(key));
+
+  return Object.fromEntries(pickedEntries);
 };
